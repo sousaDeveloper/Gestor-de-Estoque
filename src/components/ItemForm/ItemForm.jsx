8907c38bef60e275b26506f10670ae35e42c7500
Matheus Sousa <devlpsousa@gmail.com>
2023-12-19 15:49:15 -0300

    adding item table menu

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.module.scss";
+import StockItem, { CATEGORIES } from "../../entities/StockItem";
+import useStock from "../../hooks/useStock";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ItemForm.propTypes = {
   itemToUpdate: PropTypes.object,
 };
-
-const CATEGORIES = ["Jogos", "Livros", "Brinquedos", "Acessórios"];
 
 export default function ItemForm({ itemToUpdate }) {
   const defaultItem = {
@@ -18,6 +20,8 @@ export default function ItemForm({ itemToUpdate }) {
   };
 
   const [item, setItem] = useState(itemToUpdate ? itemToUpdate : defaultItem);
+  const { addItems } = useStock();
+  const inputRef = useRef(null);
 
   const handleChange = (ev) => {
     setItem((currentState) => {
@@ -28,65 +32,107 @@ export default function ItemForm({ itemToUpdate }) {
     });
   };
 
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+
+    try {
+      const validItem = new StockItem(item);
+      addItems(validItem);
+      setItem(defaultItem);
+      inputRef.current.focus();
+
+      toast.success("Produto adicionado à lista", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        theme: "dark",
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className={styles.row}>
-        <div>
-          <label htmlFor="name">Nome</label>
-          <input type="text" name="name" id="name" required value={item.name} onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="quantity">Quantidade</label>
-          <input
-            type="number"
-            name="quantity"
-            id="quantity"
-            required
-            min={0}
-            step={1}
-            value={item.quantity}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="price">Preço</label>
-          <input
-            type="number"
-            name="price"
-            id="price"
-            required
-            min={0.0}
-            step={0.01}
-            value={item.price}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="category">Categoria</label>
-          <select name="category" id="category" required value={item.category} onChange={handleChange}>
-            <option disabled value="">
-              Selecione uma categoria...
-            </option>
-            {CATEGORIES.map((category) => (
-              <option key={category} value={category} defaultChecked={item.category === category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
+        <fieldset>
+          <legend>
+            <h1>Criar Produto</h1>
+          </legend>
+          <div className={styles.gridForm}>
+            <div>
+              <label htmlFor="name">Nome</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                value={item.name}
+                onChange={handleChange}
+                ref={inputRef}
+                maxLength={20}
+                autoComplete="off"
+              />
+            </div>
+            <div>
+              <label htmlFor="quantity">Quantidade</label>
+              <input
+                type="number"
+                name="quantity"
+                id="quantity"
+                required
+                min={0}
+                step={1}
+                value={item.quantity}
+                onChange={handleChange}
+                maxLength={1000}
+              />
+            </div>
+            <div>
+              <label htmlFor="price">Preço</label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                required
+                min={0.0}
+                step={0.01}
+                value={item.price}
+                onChange={handleChange}
+                maxLength={100000}
+              />
+            </div>
+            <div>
+              <label htmlFor="category">Categoria</label>
+              <select name="category" id="category" required value={item.category} onChange={handleChange}>
+                <option disabled value="">
+                  Selecione uma categoria...
+                </option>
+                {CATEGORIES.map((category) => (
+                  <option key={category} value={category} defaultChecked={item.category === category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={styles.formControl}>
+              <label htmlFor="description">Descrição</label>
+              <input
+                name="description"
+                id="description"
+                className={styles.description}
+                required
+                value={item.description}
+                onChange={handleChange}
+                maxLength={40}
+              />
+              <button className={styles.buttonSave}>Salvar</button>
+            </div>
+          </div>
+        </fieldset>
       </div>
-      <div className={styles.formControl}>
-          <label htmlFor="description">Descrição</label>
-          <input
-            name="description"
-            id="description"
-            className={styles.description}
-            required
-            value={item.description}
-            onChange={handleChange}
-          ></input>
-          <button className={styles.buttonSave}>Salvar</button>
-        </div>
+      <ToastContainer />
     </form>
   );
 }
--- src/components/ItemsTable/ItemsTable.jsx
+++ src/components/ItemsTable/ItemsTable.jsx
@@ -8,20 +8,22 @@ export default function ItemsTable() {
   return (
     <table>
       <thead>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Em Estoque</th>
-        <th>Categoria</th>
-        <th>Ações</th>
+        <tr>
+          <th>Nº Produto </th>
+          <th>Nome</th>
+          <th>Em Estoque</th>
+          <th>Categoria</th>
+          <th>Ações</th>
+        </tr>
       </thead>
       <tbody>
         {items.map((item) => (
           <tr key={item.id}>
             <td>{item.id}</td>
             <td>{item.name}</td>
-            <td>{item.quantity}</td>
+            <td>{item.quantity} unid.</td>
             <td>{item.category}</td>
-            <td>
+            <td className="buttons">
               <Link to={`/items/${item.id}`} className={styles.buttonShow}>
                 Ver
               </Link>
